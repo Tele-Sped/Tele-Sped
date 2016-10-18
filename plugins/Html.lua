c24c2f512f0f37c6049df294406477107735c762
Tele-Sped <yazdi.mahdii001@gmail.com>
2016-10-18 08:46:16 +0330

    Create Html.lua

---
+++ plugins/Html.lua
@@ -0,0 +1,30 @@
+local function sudoteam(msg, matches)
+local mtn = matches[2]
+local link = matches[3]
+if not is_momod(msg) then
+return 'دست نزن بچه 😐❌'
+end
+if matches[1] == 'bold' then
+  return '<b>'..mtn..'</b>'
+
+elseif matches[1] == 'code' then
+  return '<code>'..mtn..'</code>'
+
+elseif matches[1] == 'hyper' then
+  return '<a href="'..link..'">'..mtn..'</a>'
+elseif matches[1] == 'italic' then
+  return '<i>'..mtn..'</i>'
+end
+end
+return {
+  description = "تست", 
+  usage = "تست",
+  patterns = {
+    "^[!#/]([Cc]ode) (.*)$",
+    "^[!#/]([Ii]talic) (.*)$",
+    "^[!#/]([Bb]old) (.*)$",
+    "^[!#/]([Hh]yper) (.*) (.*)$"
+  }, 
+  run = sudoteam 
+}
+--@tele_emoji
